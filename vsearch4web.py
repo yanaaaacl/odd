6a171bb47b9c337abac9ff1c46681755034d106b
yanaaaacl <a9506862137@yandex.ru>
2022-02-22 10:18:43 +0300

    mistake app.run, @app.route(methods=["POST"])

--- vsearch4web.py
+++ vsearch4web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
-@app.route('/search4')
+@app.route('/search4', methods=["POST"])
 def do_search() -> str:
     return str(search4letters('life, the universe, and everything', 'eiru,!'))
 @app.route('/entry') 
@@ -14,4 +14,4 @@ def entry_page() -> "html":
     return render_template("entry.html", the_title="Welcome to search4letters on the web!") 
 
 
-app.run()
+app.run(debug=True)
