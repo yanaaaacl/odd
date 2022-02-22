b055c9c55a77ed4f76c371893d57c265aa64f32e
yanaaaacl <a9506862137@yandex.ru>
2022-02-22 12:43:52 +0300

    m

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from words1 import search4letters
 
 app = Flask(__name__)
@@ -8,7 +8,9 @@ def hello() -> str:
     return 'Hello world from Flask!'
 @app.route('/search4', methods=["POST"])
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+    phrase = request.form["phrase"]
+    letters = request.form["letters"]
+    return str(search4letters(phrase, letters))
 @app.route('/entry') 
 def entry_page() -> "html":
     return render_template("entry.html", the_title="Welcome to search4letters on the web!") 
