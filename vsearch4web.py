a0081b2943fe84268df2842d6ab96b22d4585ff5
yanaaaacl <a9506862137@yandex.ru>
2022-02-24 10:10:11 +0300

    new finction

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from words1 import search4letters
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+def hello() -> '302':
+    return redirect('/entry')
 @app.route('/search4', methods=["POST"])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form["phrase"]
     letters = request.form["letters"]
-    return str(search4letters(phrase, letters))
+    title = "Here are your results:"
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                            the_phrase=phrase,
+                            the_letters=letters,
+                              the_title=title,
+                               the_results=results,)
 @app.route('/entry') 
 def entry_page() -> "html":
     return render_template("entry.html", the_title="Welcome to search4letters on the web!") 
